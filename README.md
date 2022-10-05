336ecc037ddc63b2acb3d8618bcd9fce8fd4dc7f
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-05 14:55:37 +0300

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# INFINITE SCROLL COMPONENT FOR IONIC APP
+
+* This is a simple implementation of an infinite scroll component is ionic angular mobile application. 
+
+## Screenshots
+
+<img src="./src/assets/screen.gif" alt="screenshot">
+
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  users = [];
+  page = 1;
+  maximumPages = 5;
+  constructor(private http: HttpClient) {
+    this.loadUsers();
+  }
+  loadUsers(event?) {
+    const url = `https://randomuser.me/api/?results=20&page=${this.page}`;
+    this.http.get(url).subscribe((res: any) => {
+      console.log(res);
+      this.users = this.users.concat(res.results);
 
-  constructor() {}
-
+      if(event){
+        event.target.complete();
+      }
+    });
+  }
+  loadData(event){
+    console.log(event);
+    this.page ++;
+    this.loadUsers(event);
+    if(this.page === this.maximumPages){
+      event.target.disabled = true;
+    }
+  }
 }
