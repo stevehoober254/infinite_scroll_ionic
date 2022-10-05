cd13e99e0619d609ee1971a98ad140f09614ef16
Stephen Gachoka <stephengachoka57@gmail.com>
2022-10-05 13:20:29 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Infinite_scroll',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
